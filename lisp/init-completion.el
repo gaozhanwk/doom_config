080db760040434f0ed3e113b6bbb5a28822f3b8f
gaozhan <gaozhanwk@163.com>
2023-09-14 16:31:11 +0800

    添加init-ui init-edit init-org init-completion
    Signed-off-by: gaozhan <gaozhanwk@163.com>

---
+++ lisp/init-completion.el
@@ -0,0 +1,8 @@
+;;; init-completion.el --- Completion settings -*- lexical-binding: t -*-
+;;; Commentary:
+
+;;; Code:
+
+(provide 'init-completion)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-completion.el ends here
---
+++ lisp/init-ui.el
@@ -0,0 +1,8 @@
+;;; init-ui.el --- UI settings -*- lexical-binding: t -*-
+;;; Commentary:
+
+;;; Code:
+
+(provide 'init-ui)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-ui.el ends here
